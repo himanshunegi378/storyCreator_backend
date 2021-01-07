6859520fc1d8cc0c47ccc81afd6ab2c0cbcfb90b
himanshunegi378 <davidxavier378@gmail.com>
2021-01-07 16:11:12 +0530

    Added body parser middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import "reflect-metadata";
 import express from "express";
 import { createConnection, getRepository } from "typeorm";
-import { User } from "./entity/User";
 import cors from "cors";
 import { Fragment } from "./entity/Fragment";
 import { Section } from "./entity/Section";
 import { Book } from "./entity/Book";
+import bodyParser from 'body-parser'
+
 
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 const connection = (async function () {
   return await createConnection({
@@ -23,34 +25,6 @@ const connection = (async function () {
     synchronize: true,
   });
 })();
-// createConnection({
-//   type: "mysql",
-//   host: "127.0.0.1",
-//   port: 3306,
-//   username: "root",
-//   password: "3rdMAY1998@",
-//   database: "storycreator",
-//   entities: [__dirname + "/enconsole.log('called')tity/*.ts"],
-//   synchronize: true,
-// })
-//   .then(async (connection) => {
-//     console.log("Inserting a new user into the database...");
-
-//     const fragment1 = new Fragment();
-//     fragment1.text = "Timber";
-//     fragment1.like = 10;
-//     // await connection.manager.save(fragment1);
-
-//     const fragment2 = new Fragment();
-//     fragment2.text = "Timber";
-//     fragment2.like = 10;
-//     // await connection.manager.save(fragment1);
-
-//     const section = new Section();
-//     section.fragments = [fragment1, fragment2];
-//     const users = await connection.manager.find(section);
-//   })
-//   .catch((error) => console.log(error));
 
 app.get("/", (req: express.Request, res: express.Response) =>
   res.send("Express + TypeScript Server")
