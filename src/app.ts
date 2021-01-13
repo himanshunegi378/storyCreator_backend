bf700dc5e28c9def2cd985a01fa5a093d3dcfc0e
himanshunegi378 <davidxavier378@gmail.com>
2021-01-13 19:57:20 +0530

    little cleanup here and there

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,14 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import bookRoutes from "./routes/book.route";
-import init from "./entity/connection";
 import sectionRoutes from "./routes/section.route";
 import fragmentRoutes from "./routes/fragment.route";
 import "./entity/firestore";
-init();
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/", (req: express.Request, res: express.Response) =>
-  res.send("Express + TypeScript Server")
-);
 bookRoutes(app);
 sectionRoutes(app);
 fragmentRoutes(app);
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,16 @@
 import DatabaseImpl from "./databseImpl";
 
 let database: DatabaseImpl;
-function init(databasetype: new () => DatabaseImpl) {
+/**
+ * only one type of database could be used once executed
+ *
+ * @param concreteDatabase typeof database eg: sql,firestore,mongodb from where data neeed to transmitted
+ */
+function init(concreteDatabase: new () => DatabaseImpl) {
   if (database) {
     throw new Error("Database already initalized");
   }
-  database = new databasetype();
+  database = new concreteDatabase();
 }
 
 function getDatabase() {
