c869b657c71167c5c8231f4fdf87edb806de6319
himanshunegi378 <davidxavier378@gmail.com>
2021-01-15 11:06:53 +0530

    sd

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import bodyParser from "body-parser";
 import bookRoutes from "./routes/book.route";
 import sectionRoutes from "./routes/section.route";
 import fragmentRoutes from "./routes/fragment.route";
-import "./entity/firestore";
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
