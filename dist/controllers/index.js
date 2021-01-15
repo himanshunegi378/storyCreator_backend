fced27312d12c23a09b11a3a5d6f9118f40c6dea
himanshunegi378 <davidxavier378@gmail.com>
2021-01-15 21:47:45 +0530

    dist folder deleted

--- dist/controllers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var book_controller_1 = require("./book.controller");
-exports.BookController = book_controller_1.BookController;
-var fragment_controller_1 = require("./fragment.controller");
-exports.Fragmentcontroller = fragment_controller_1.Fragmentcontroller;
-var section_controller_1 = require("./section.controller");
-exports.SectionController = section_controller_1.SectionController;
